a211cfbd1ebcdc51304644bc04ecf7921ac0a841
Prakash Aryal <prakasbabuaryal@gmail.com>
2023-10-18 20:23:53 -0400

    endpoint

--- IMS/UserMicroservice/src/main/java/com/takeo/controller/UserEntityController.java
+++ IMS/UserMicroservice/src/main/java/com/takeo/controller/UserEntityController.java
@@ -3,6 +3,7 @@ package com.takeo.controller;
 import com.takeo.entity.UserEntity;
 import com.takeo.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/home/user")
 public class UserEntityController {
     private final UserServiceImpl userService;
+
+    @PostMapping("/create")
     public String createUser(@RequestBody UserEntity userEntity){
         UserEntity createdUser = userService.createUser(userEntity);
         if (createdUser != null){
